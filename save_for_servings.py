108cdc5988295cf77cd0a95665aa85221d14d840
shehand <sonodewcomplex@gmail.com>
2019-05-09 13:48:03 +0530

    update serve model

--- save_for_servings.py
+++ save_for_servings.py
@@ -5,17 +5,19 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-MODEL_DIR = tempfile.gettempdir()
-version = 2
-export_path = os.path.join(MODEL_DIR, str(version))
-print('export_path = {}\n'.format(export_path))
-if os.path.isdir(export_path):
-  print('\nAlready saved a model, cleaning up\n')
+def generate_serve_files(model):
 
-tf.saved_model.simple_save(
-    keras.backend.get_session(),
-    export_path,
-    inputs={'input_image': model.input},
-    outputs={t.name:t for t in model.outputs})
+	MODEL_DIR = tempfile.gettempdir()
+	version = 2
+	export_path = os.path.join(MODEL_DIR, str(version))
+	print('export_path = {}\n'.format(export_path))
+	if os.path.isdir(export_path):
+	  print('\nAlready saved a model, cleaning up\n')
 
-print('\nSaved model:')
+	tf.saved_model.simple_save(
+	    keras.backend.get_session(),
+	    export_path,
+	    inputs={'input_image': model.input},
+	    outputs={t.name:t for t in model.outputs})
+
+	print('\nSaved model:')
