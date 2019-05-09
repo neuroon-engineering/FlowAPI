c12d9b8b1eb03d2717c9cca4507a4b46778a3c83
shehand <sonodewcomplex@gmail.com>
2019-05-09 13:46:42 +0530

    add code for tf-serving process

---
+++ save_for_servings.py
@@ -0,0 +1,21 @@
+import tempfile
+import os
+import subprocess
+import tensorflow as tf
+from tensorflow import keras
+
+
+MODEL_DIR = tempfile.gettempdir()
+version = 2
+export_path = os.path.join(MODEL_DIR, str(version))
+print('export_path = {}\n'.format(export_path))
+if os.path.isdir(export_path):
+  print('\nAlready saved a model, cleaning up\n')
+
+tf.saved_model.simple_save(
+    keras.backend.get_session(),
+    export_path,
+    inputs={'input_image': model.input},
+    outputs={t.name:t for t in model.outputs})
+
+print('\nSaved model:')
