6ff15ad95ca10bfdc40e9f64ed69b243e8f20e12
shehand <sonodewcomplex@gmail.com>
2019-05-09 13:51:23 +0530

    add method to save trained model as .h5 extention

---
+++ save_as_model.py
@@ -0,0 +1,19 @@
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import os
+import subprocess
+import tensorflow as tf
+from tensorflow import keras
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+model.save_weights('first_try.h5')
