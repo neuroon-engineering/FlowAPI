6e25a4a309c0d6549c742463784aed4f3254ef1a
shehand <sonodewcomplex@gmail.com>
2019-05-09 13:52:05 +0530

    add data_model.py to generate trained and test data samples from the given imaegs

--- data_model.py
+++ data_model.py
@@ -18,35 +18,6 @@ nb_validation_samples = 218
 epochs = 1
 batch_size = 16
 
-if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
-else:
-    input_shape = (img_width, img_height, 3)
-
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-model.compile(loss='binary_crossentropy',
-              optimizer=tf.train.AdamOptimizer(),
-              metrics=['accuracy'])
-
 # this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
@@ -70,28 +41,6 @@ validation_generator = test_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='binary')
 
-model.fit_generator(
-    train_generator,
-    steps_per_epoch=nb_train_samples // batch_size,
-    epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size)
 
-model.save_weights('first_try.h5')
 
-import tempfile
 
-MODEL_DIR = tempfile.gettempdir()
-version = 2
-export_path = os.path.join(MODEL_DIR, str(version))
-print('export_path = {}\n'.format(export_path))
-if os.path.isdir(export_path):
-  print('\nAlready saved a model, cleaning up\n')
-
-tf.saved_model.simple_save(
-    keras.backend.get_session(),
-    export_path,
-    inputs={'input_image': model.input},
-    outputs={t.name:t for t in model.outputs})
-
-print('\nSaved model:')
